666e901992c886b7ed306a4b93d30a99d3257fd6
Vivian Breckel <vivianbreckel@gmail.com>
2021-11-23 13:17:01 +0100

    addedk
    clear

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -1,16 +1,20 @@
 alert('Grundschulmathe');
 
-const a = Math.floor(Math.random() * 21);
-const b = Math.floor(Math.random() * 21);
+while(i != 5){
+    const a = Math.floor(Math.random() * 21);
+    const b = Math.floor(Math.random() * 21);
 
-let ergebnisAddition = prompt("Wie viel ist " + a + " + " + b + "?");
-console.log(ergebnisAddition);
-document.write(a + " + " + b + " = " + ergebnisAddition + "<br>");
+    let ergebnisAddition = prompt("Wie viel ist " + a + " + " + b + "?");
+    console.log(ergebnisAddition);
+    document.write(a + " + " + b + " = " + ergebnisAddition + "<br>");
 
-    if (a + b == ergebnisAddition) {
-        punkteAddition = punkteAddition + 1;
-        alert("Richitg gerechnet!");
-    }
-    else {
-        alert("Falsch gerechnet!");
-    }
+        if (a + b == ergebnisAddition) {
+            punkteAddition = punkteAddition + 1;
+            alert("Richitg gerechnet! Ihr Punktestand: " + punkteAddition + "/5");
+        }
+        else {
+            alert("Falsch gerechnet! Ihr Punktestand: " + punkteAddition + "/5");
+        }
+
+    i++;
+}
