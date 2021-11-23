617486ac0f12de1fe7996bd1157231778fdcf170
Vivian Breckel <vivianbreckel@gmail.com>
2021-11-23 12:57:35 +0100

    added addition to grundschulmathe.js

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -1,1 +1,16 @@
 alert('Grundschulmathe');
+
+const a = Math.floor(Math.random() * 21);
+const b = Math.floor(Math.random() * 21);
+
+let ergebnisAddition = prompt("Wie viel ist " + a + " + " + b + "?");
+console.log(ergebnisAddition);
+document.write(a + " + " + b + " = " + ergebnisAddition + "<br>");
+
+    if (a + b == ergebnisAddition) {
+        punkteAddition = punkteAddition + 1;
+        alert("Richitg gerechnet!");
+    }
+    else {
+        alert("Falsch gerechnet!");
+    }
